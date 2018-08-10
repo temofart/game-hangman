cef311669612b8ee5dd9618f3d24d7cb30043227
Artyom O <18art12@gmail.com>
2018-08-10 06:51:50 +0300

    new game && timeout && usedWords

--- app/js/main.js
+++ app/js/main.js
@@ -1,37 +1,55 @@
-let words = ["apple", "notebook", "water", "clock", "country"]
-let word = words[Math.floor(Math.random() * words.length)].toUpperCase()
-
-let mainWord = []
-for(let j = 0; j < word.length; j++) {
-  mainWord[j] = " _ "
+let isPlaying = false
+let play = document.getElementById('startPlay')
+play.onclick = function() {
+  isPlaying = true
+  startGame()
 }
+function startGame() {
+  if (isPlaying) {
+    let words = ["apple", "notebook", "water", "clock", "country"]
+    let word = words[Math.floor(Math.random() * words.length)].toUpperCase()
 
-let countLetters = word.length
-
-let getValue
-let wordOutput = document.getElementById('wordOutput')
-wordOutput.innerHTML = mainWord.join('')
-
-let userInput = document.getElementById('inputWord')
-let submit = document.getElementById('submit')
-
-
-submit.onclick = function() {
-  if (countLetters > 0) {
-    getValue = userInput.value.toUpperCase()
-
-    for(let i = 0; i < word.length; i++) {
-      if (getValue === word[i]) {
-        mainWord[i] = getValue;
-        countLetters--
-      }
+    let mainWord = []
+    for(let j = 0; j < word.length; j++) {
+      mainWord[j] = " _ "
     }
-    userInput.value = ""
+
+    let getValue
+    let wordOutput = document.getElementById('wordOutput')
     wordOutput.innerHTML = mainWord.join('')
-    if (countLetters == 0) {
-      wordOutput.innerHTML = 'You Win!'
-      submit.classList.add('green')
-      submit.innerText = "Again!"
+
+    let userInput = document.getElementById('inputWord')
+    let submit = document.getElementById('submit')
+
+    let countLetters = word.length
+    let usedWords = []
+
+
+    submit.classList.remove('green')
+    submit.innerText = "Check"
+
+    submit.onclick = function() {
+      userInput.focus()
+      if (countLetters > 0) {
+        getValue = userInput.value.toUpperCase()
+
+        for(let i = 0; i < word.length; i++) {
+          if (getValue === word[i] && getValue !== usedWords[i]) {
+            mainWord[i] = getValue;
+            usedWords.push(getValue)
+            countLetters--
+          }
+        }
+        userInput.value = ""
+        wordOutput.innerHTML = mainWord.join('')
+        if (countLetters == 0) {
+          setTimeout(function youWin() {
+            wordOutput.innerHTML = 'You Win!'
+            submit.classList.add('green')
+            submit.innerText = "Again!"
+          }, 200)
+        }
+      }
     }
   }
 }
